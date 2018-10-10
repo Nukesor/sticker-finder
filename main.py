ab0511e86668161bc88435e1c129564db4abb125
Arne Beer <arne@twobeer.de>
2018-10-10 20:09:15 +0200

    Use url instead of webhook_url

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from stickerfinder.config import config
 
 if config.WEB_HOOK:
     updater.start_webhook(listen='127.0.0.1', port=config.PORT, url_path=config.TOKEN)
-    updater.bot.set_webhook(webhook_url=f'{config.DOMAIN}{config.TOKEN}',
+    updater.bot.set_webhook(url=f'{config.DOMAIN}{config.TOKEN}',
                             certificate=open(config.CERT_PATH, 'rb'))
 else:
     updater.start_polling()
