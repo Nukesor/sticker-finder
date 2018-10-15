4f296538c3becf1b524427f7e73e29139a4b8b02
Arne Beer <arne@twobeer.de>
2018-10-15 15:03:44 +0200

    Better backup

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 host=nuke@jarvis
-timestamp=`date +%Y%m%d`
+timestamp=`date +%Y-%m-%d_%H-%m`
 dest="backup/stickerfinder/stickerfinder_${timestamp}.dump"
 
 ssh $host "mkdir -p ~/backup/stickerfinder"
