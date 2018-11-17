89bc0cb82af4d86ac2a120ccb6debc78c40d06d2
Arne Beer <arne@twobeer.de>
2018-11-17 14:02:18 +0100

    Fix error handling in newsfeed callback

--- stickerfinder/telegram/callback.py
+++ stickerfinder/telegram/callback.py
@@ -140,15 +140,13 @@ def handle_callback_query(bot, update, session, user):
 
         task.reviewed = True
         sticker_set.reviewed = True
-        session.commit()
 
-        try:
-            keyboard = get_nsfw_ban_keyboard(sticker_set)
-            call_tg_func(query.message, 'edit_reply_markup', [], {'reply_markup': keyboard})
-            task_chat = task.processing_chat[0]
-            distribute_newsfeed_tasks(bot, session, [task_chat])
-        except:
-            pass
+        task_chat = task.processing_chat[0]
+        distribute_newsfeed_tasks(bot, session, [task_chat])
+        keyboard = get_nsfw_ban_keyboard(sticker_set)
+        call_tg_func(query.message, 'edit_reply_markup', [], {'reply_markup': keyboard})
+
+        session.commit()
 
         if task_chat.current_task is None:
             call_tg_func(query, 'answer', ['No new stickers sets'])
